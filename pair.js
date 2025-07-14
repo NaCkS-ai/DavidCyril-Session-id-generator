67bbb98d01e9dff4d8bedf7687141d7e9ebbe707
SungSu-ho <ryanbaartman12@gmail.com>
2025-07-14 12:45:40 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by ZENITSU*
-*_Made With 🚬🗿_*
+*_Pair Code Connected by Drakonis-MD*
+*_Made With 💓 by Dev Sung_*
 ______________________________________
 ╔════◇
-║ *『 AMAZING YOU'VE CHOSEN ZENITSU CRASH V2 』*
+║ *『 AMAZING YOU'VE CHOSEN Drakonis-MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _https://www.youtube.com/@BTSMODZ
-║❒ *Owner:* https://wa.me/2348075952205_
-║❒ *Repo:* _https://github.com/Fearless-tech1_
-║❒ *WaGroup:* _https://chat.whatsapp.com/C3GFThC0tIpGaJY9DFUeCK
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VahusSh0QeaoFzHJCk2x
-║❒ *Plugins:* _https://github.com/Fearless-tech1 
+║❒ *Ytube:* _https://youtube.com/@malvintech2
+║❒ *Owner:* https://wa.me/1(236)362-1958_
+║❒ *Repo:* _https://github.com/NaCkS-ai/Drakonis-MD_
+║❒ *WaGroup:* _https://chat.whatsapp.com/IuA7gCYXCQ1GxMYLgnX4qG?mode=r_c
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbB3YxTDJ6H15SKoBv3S
+║❒ *Plugins:* _https://github.com/NaCkS-ai/Drakonis-MD 
 ╚════════════════════════╝
 _____________________________________
 
